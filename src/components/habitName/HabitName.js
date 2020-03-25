fbfeb8c2d236eaa40a883631a9feb31d19e0847a
PickleDan <pickledan@yandex.ru>
2020-03-25 22:51:50 +0300

    add editing mode for habits names and habits measures

--- src/components/habitName/HabitName.js
+++ src/components/habitName/HabitName.js
@@ -2,13 +2,27 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
 import HabitNameStyles from "./HabitName.module.scss";
+import cn from "classnames";
 
 const HabitName = ({
     habit,
-    inputState,
     handleHabitNameOnChange,
-    handleHabitNameOnClick
+    handleHabitNameOnClick,
+    editingMode,
+    onBlurHabitName
 }) => {
+    const icons = (
+        <>
+            <FontAwesomeIcon
+                className={HabitNameStyles.habitNameIconCheck}
+                icon={faCheck}
+            />
+            <FontAwesomeIcon
+                className={HabitNameStyles.habitNameIconDeny}
+                icon={faTimes}
+            />
+        </>
+    );
     return (
         <td>
             <form className={HabitNameStyles.habitNameForm}>
@@ -16,19 +30,12 @@ const HabitName = ({
                     className="habit-name-input"
                     value={habit.name}
                     onChange={e => handleHabitNameOnChange(e, habit)}
-                    onClick={e => handleHabitNameOnClick(e, "habitName")}
+                    onClick={() => handleHabitNameOnClick(habit)}
+                    onBlur={onBlurHabitName}
                 ></input>
-                <div
-                    className={` ${HabitNameStyles.habitNameButtons}, ${HabitNameStyles.habitNameButtonsHidden}`}
-                >
-                    <FontAwesomeIcon
-                        className={HabitNameStyles.habitNameIconCheck}
-                        icon={faCheck}
-                    />
-                    <FontAwesomeIcon
-                        className={HabitNameStyles.habitNameIconDeny}
-                        icon={faTimes}
-                    />
+
+                <div className={cn(HabitNameStyles.habitNameButtons)}>
+                    {editingMode[habit.id - 1] ? icons : undefined}
                 </div>
             </form>
         </td>
